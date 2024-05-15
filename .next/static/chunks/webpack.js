172154a93da36086e260bc621e263904e7ff0f95
Olivier <omalahel@gmail.com>
2024-05-15 23:24:17 +0200

    update front

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "b017cb87bd8cb4c2"; }
+/******/ 		__webpack_require__.h = function() { return "665e70e693a2c718"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
